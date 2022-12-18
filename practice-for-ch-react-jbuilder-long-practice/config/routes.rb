db380b30f3d25ea0d7e13ffb1a9d3ff13f5fd6e1
celuu <christine.e.luu@gmail.com>
2022-12-18 13:09:37 -0800

    Finished Assignment

--- practice-for-ch-react-jbuilder-long-practice/config/routes.rb
+++ practice-for-ch-react-jbuilder-long-practice/config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, defaults: {format: :json} do 
+    resources :guests, only: [:show, :index] do
+      resources :gifts, only: [:index]
+    end
+    resources :gifts, only: [:show] 
+    resources :parties, only: [:show, :index]
+  end
+
 end
